09726b216815e971ef4ac8df49930c27095f9953
FrancesJoan <98513430+FrancesJoan@users.noreply.github.com>
2022-05-13 18:07:27 +0800

    Add files via upload

---
+++ Script15.js
@@ -0,0 +1,20 @@
+var myName="Frances Joan Siddayao"
+var myAge="20"
+var myPhoneNumber="09163127482"
+var myEmail="20209117@s.ubaguio.edu"
+var myAddress="Transmitter Bangued Abra"
+var myProgram="BSIT"
+var myID="20209117"
+var myYear="2nd Year"
+var mySection="IDA2"
+
+console.log('Hello');
+console.log('First Name: ' + myName);
+console.log('Age: ' + myAge);
+console.log('Phone Number: ' + myPhoneNumber);
+console.log('Email Address: ' + myEmail);
+console.log('Mailing Address: ' + myAddress);
+console.log('Program: ' + myProgram);
+console.log('ID Number: ' + myID);
+console.log('Year Level: ' + myYear);
+console.log('Section: ' + mySection);
